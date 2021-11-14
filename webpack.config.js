60e55d7c2a9892670de3518a8524df953b81883a
Fushihara <1039534+fushihara@users.noreply.github.com>
2021-11-14 17:18:27 +0900

    manifestにexcludeを追加

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,23 @@
 const webpack = require('webpack');
 const path = require("path");
 module.exports = {
-  mode: 'development',
-  entry: './src/index.ts',
+  mode: "development",
+  entry: "./src/index.ts",
   devtool: "inline-source-map",
   module: {
     rules: [
       {
         test: /\.ts$/,
-        use: "ts-loader"
-      }
+        use: "ts-loader",
+      },
     ],
   },
   output: {
     filename: "script.user.js",
-    path: path.resolve(__dirname, 'out'),
+    path: path.resolve(__dirname, "out"),
   },
   resolve: {
-    extensions: ['.ts', '.js',],
+    extensions: [".ts", ".js"],
   },
   plugins: [
     new webpack.BannerPlugin({
@@ -25,6 +25,7 @@ module.exports = {
       // @name         esa.io copy menu
       // @namespace    https://github.com/fushihara/esa-io-copy-menu
       // @match        https://*.esa.io/posts/*
+      // @exclude      https://*.esa.io/posts/*/edit
       // @description  esa.io PC版のドロップダウンメニューに、タイトル・URLを個別にコピーする機能を追加
       // @version      ${process.env.npm_package_version}
       // @grant        none
@@ -32,8 +33,11 @@ module.exports = {
       // @source       https://github.com/fushihara/esa-io-copy-menu
       // @homepage     https://greasyfork.org/ja/scripts/410893-esa-io-copy-menu
       // @noframes
-      // ==/UserScript==`.split("\n").map(a => a.trim()).join("\n"),
+      // ==/UserScript==`
+        .split("\n")
+        .map((a) => a.trim())
+        .join("\n"),
       raw: true,
-    })
-  ]
+    }),
+  ],
 };
